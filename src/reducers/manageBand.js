1a619216d6819b0b019be7db6071f7cf089a5a00
andrew041994 <andrew041994@gmail.com>
2020-06-08 00:22:58 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,12 @@
 export default function manageBand(state = {
   bands: [],
 }, action) {
-  return state
+  switch (action.type) {
+    case "ADD_BAND":
+      let newState = {bands:[...state.bands,action.band]}
+       return newState
+    default:
+      return state
+  }
+  
 };
